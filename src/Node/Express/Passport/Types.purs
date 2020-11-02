a67feb94b7a3945554c850b6dfd60dc264e14816
Serhii Khoma <srghma@gmail.com>
2020-11-02 19:44:11 +0200

    feat: newtype StrategyId

--- src/Node/Express/Passport/Types.purs
+++ src/Node/Express/Passport/Types.purs
@@ -1,7 +1,11 @@
 module Node.Express.Passport.Types where
 
+import Data.Newtype (class Newtype)
+
 data Passport
 
 data PassportStrategy
 
 newtype StrategyId = StrategyId String
+
+derive instance newtypeStrategyId :: Newtype StrategyId _
